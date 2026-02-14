d1b1e08d1bf8eca2fbb82d9de008a9a01c56d1de
yating1901 <zhengyating@mail.bnu.edu.cn>
2026-02-14 19:54:06 +0100

    Nh=4,5 for  Ns=60 in anta

--- run_anta_simulation.sh
+++ run_anta_simulation.sh
@@ -16,9 +16,9 @@ for alpha in "${alphas[@]}"
 do
   Alpha=$(printf "%.3f" "$alpha")
   echo "Alpha = $Alpha"
-  for N_shepherd in $(seq 3 1 4)
+  for N_shepherd in $(seq 4 1 5)
   do
-    for N_sheep in $(seq 120 20 120)
+    for N_sheep in $(seq 60 20 60)
     do
       for Repetition in $(seq 1 1 5)
       do
