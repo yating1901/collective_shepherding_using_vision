9e3da8b38f391ac706889cd6132bba4dd4bb6154
yating1901 <zhengyating@mail.bnu.edu.cn>
2026-02-17 12:41:23 +0100

    add run_explicit.sh

--- main_explicit.py
+++ main_explicit.py
@@ -1,37 +1,43 @@
-import os,glob
+import argparse
+from loop_function import Loop_Function
+import os, glob, json
 import numpy as np
+
+parser = argparse.ArgumentParser()
+parser.add_argument("n_sheep", type=int)
+parser.add_argument("n_shepherd", type=int)
+parser.add_argument("iterations", type=int)
+parser.add_argument("l3", type=int)
+parser.add_argument("repetition", type=int)
+args = parser.parse_args()
 
 Target_place_x = 800
 Target_place_y = 800
-Target_size = 200  # radius
-
+Target_size = 200
 Boundary_x = Target_place_x + Target_size
 Boundary_y = Target_place_y + Target_size
 
 
-TICK = 1
-Iterations = 10000
-
 N_sheep = 100
-N_shepherd = 2
+N_shepherd = 1
 L3 = 100   # minimum repulsion distance with other shepherds;
 Fps = 50
 Uncomfortable_distance = 500
 Is_Explicit = True
 Robot_Loop = False
-Save_data = False
+Save_data = True
 Show_Animation = False
-Is_Visualized = True #False
+Is_Visualized = False
 Is_Antagonistic = True
 Alpha = np.pi/6 #5 4 3
 Coll_threshold = np.pi / 2
 Drive_threshold = np.pi / 6
 
-from loop_function import Loop_Function
 
-loop_function = Loop_Function(N_sheep=N_sheep,  # Number of agents in the environment
-                              N_shepherd = N_shepherd,
-                              Time=Iterations,  # Simulation timesteps
+
+loop_function = Loop_Function(N_sheep=args.n_sheep,
+                              N_shepherd = args.n_shepherd,
+                              Time=args.iterations,  # Simulation timesteps
                               width=Boundary_x,  # Arena width in pixels
                               height=Boundary_y,  # Arena height in pixels
                               target_place_x = Target_place_x,
@@ -42,7 +48,7 @@ loop_function = Loop_Function(N_sheep=N_sheep,  # Number of agents in the enviro
                               with_visualization = Is_Visualized,
                               show_animation = Show_Animation,
                               agent_radius= 10,  # 10 Agent radius in pixels
-                              L3 = L3,  # repulsion distance
+                              L3 = args.l3,  # repulsion distance
                               robot_loop = Robot_Loop,
                               physical_obstacle_avoidance=False,
                               uncomfortable_distance = Uncomfortable_distance,
@@ -53,27 +59,4 @@ loop_function = Loop_Function(N_sheep=N_sheep,  # Number of agents in the enviro
                               angle_threshold_collection = Coll_threshold,
                               angle_threshold_drive = Drive_threshold)
 
-# we loop through all the agents of the created simulation
-# print("Setting parameters for agent", end = ' ')
-
-
-folder_path = os.getcwd() + "/snapshots"
-pngs = glob.glob(os.path.join(folder_path, "*.png"))
-for file_path in pngs:
-    os.remove(file_path)
-
-folder_path = os.getcwd() + "/projections"
-pngs = glob.glob(os.path.join(folder_path, "*.png"))
-for file_path in pngs:
-    os.remove(file_path)
-
-with open('shepherd_agent_data.json', 'w') as f:
-    f.write('')
-
-with open('sheep_agent_data.json', 'w') as f:
-    f.write('')
-# Now we can start the simulation with the changed agents
 loop_function.start()
-
-# make video from pngs
-#ffmpeg -framerate 100 -i %d.png -c:v libx264 -r 30 -pix_fmt yuv420p output.mp4
---
+++ run_explicit.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+BASE_OUTPUT="/mnt/DATA/yating/results/explicit"
+mkdir -p "$BASE_OUTPUT"
+export LC_NUMERIC=C  # Force C locale (uses dot as decimal separator)
+
+JOBS=()
+Iterations=50000
+Is_explcit=True
+
+for L3 in $(seq 0 100 101)
+do
+  for N_shepherd in $(seq 2 1 3)
+  do
+    for N_sheep in $(seq 50 50 200)
+    do
+      for Repetition in $(seq 1 1 5)
+      do
+        OUTPUT_FOLDER="$BASE_OUTPUT/L3_$L3/N_shepherd_$N_shepherd/N_sheep_$N_sheep/rep_$Repetition"
+        mkdir -p "$OUTPUT_FOLDER"
+        # Store the job as a string: "A REP OUTPUT_FOLDER"
+        JOBS+=("$N_sheep $N_shepherd $Iterations $L3 $Repetition $OUTPUT_FOLDER")
+      done
+    done
+  done
+done
+
+
+i=0
+for JOB in "${JOBS[@]}"; do
+    # Parse fields: $A $REP $OUTPUT_FOLDER
+    set -- $JOB
+    N_sheep=$1
+    N_shepherd=$2
+    Iterations=$3
+    L3=$4
+    Repetition=$5
+    OUTPUT_FOLDER=$6
+
+    mkdir -p "$OUTPUT_FOLDER"
+
+    echo "Launching job N_sheep=$N_sheep rep=$Repetition Iterations=$Iterations L3=$L3 Repetition=$Repetition"
+    # In your .sh file, before running python
+    export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH"
+    nohup env OUTPUT_DIR="$OUTPUT_FOLDER" python main_anta.py $N_sheep $N_shepherd $Iterations $L3 $Repetition > "$OUTPUT_FOLDER/output.txt" 2>&1 < /dev/null &
+    i=$((i+1))
+done
+
+#wait
+echo "Number of total runs: $i"
+
