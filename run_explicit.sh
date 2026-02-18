7035f30d14732c10881336efff661669cddee423
yating1901 <zhengyating@mail.bnu.edu.cn>
2026-02-18 15:50:46 +0100

    debug

--- run_explicit.sh
+++ run_explicit.sh
@@ -41,7 +41,7 @@ for JOB in "${JOBS[@]}"; do
     echo "Launching job N_sheep=$N_sheep rep=$Repetition Iterations=$Iterations L3=$L3 Repetition=$Repetition"
     # In your .sh file, before running python
     export LD_LIBRARY_PATH="/usr/lib/x86_64-linux-gnu:/usr/lib:$LD_LIBRARY_PATH"
-    nohup env OUTPUT_DIR="$OUTPUT_FOLDER" python main_complex.py $N_sheep $N_shepherd $Iterations $L3 $Repetition > "$OUTPUT_FOLDER/output.txt" 2>&1 < /dev/null &
+    nohup env OUTPUT_DIR="$OUTPUT_FOLDER" python main_explicit.py $N_sheep $N_shepherd $Iterations $L3 $Repetition > "$OUTPUT_FOLDER/output.txt" 2>&1 < /dev/null &
     i=$((i+1))
 done
 
